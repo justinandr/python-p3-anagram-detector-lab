2d1d306fc980e07131565eace9e8877a16785a2f
Justin Rodriguez <justin.r@me.com>
2024-05-07 21:48:57 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word)
+
+    def match(self, list):
+        matches = []
+
+        for word in list:
+            if sorted(word) == self.word:
+                matches.append(word)
+
+        return matches
